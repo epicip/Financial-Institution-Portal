a9d5b36b37ff4f2feea7f8db882505fd94e4108c
rahulepe <rahul.mishra@epe-reynell.co.uk>
2026-08-20 16:18:34 +0100

    Code update

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -15,11 +15,16 @@ class Dashboard extends My_Controller
 
 		$this->load->helper(array('cookie', 'date', 'form'));
 		$this->load->library(array('form_validation'));
-		$this->load->model('dashboard_model');
+		$this->load->model('cases_model');
 	}
 
 	function index()
 	{
+		$all_cases = $this->cases_model->get_all_cases();
+		$pending_cases = $this->cases_model->get_pending_cases();
+
+		$this->data['all_cases'] = is_array($all_cases) ? $all_cases : array();
+		$this->data['pending_cases'] = is_array($pending_cases) ? $pending_cases : array();
 		$this->load->view('dashboard', $this->data);
 	}
 }
